a8077a33c1518df8daf86c62a5d1e1e3e3f7e741
Chiv Timeng <ctimeng@vodworks.com>
2024-09-04 17:31:11 +0700

    updated

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,24 +1,3 @@
-declare enum MobileOS {
-    unknown = "Unknown",
-    android = "Android",
-    ios = "iOS",
-    window = "Window"
-}
-declare const metaMaskExtensionUrls: {
-    chrome: string;
-    firefox: string;
-    edge: string;
-};
-declare const walletAppUrls: {
-    metamask: string;
-};
+declare const isWalletInstalled: (providerName: string) => boolean;
 
-declare enum ProviderOption {
-    metamask = "MetaMask",
-    walletConnect = "WalletConnect",
-    coinbaseWallet = "Coinbase Wallet",
-    internal = "Internal",
-    notConnected = "notConnected"
-}
-
-export { MobileOS, ProviderOption, metaMaskExtensionUrls, walletAppUrls };
+export { isWalletInstalled };
