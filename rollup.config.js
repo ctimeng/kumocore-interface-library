e9fac7a242d41f42f091170af5d4d4dc2c0063bd
Chiv Timeng <ctimeng@vodworks.com>
2024-09-04 20:27:43 +0700

    1.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -25,10 +25,6 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
-  },
-  {
-    input: "dist/esm/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
+    external: ['react', 'react-dom']
   },
 ];
